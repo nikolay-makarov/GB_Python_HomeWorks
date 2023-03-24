7ea278a24c700f160cfa66f8fe9a1f4e6ea2eba2
Nicolay Makarov <i.n.makarov@mail.ru>
2023-03-25 00:06:32 +0500

    Пятое домашнее задание

---
+++ HomeWork005/Task_extra.py
@@ -0,0 +1,17 @@
+"""
+Проверка палиндрома. Напишите функцию, которая проверяет,
+является ли строка палиндромом (то есть читается одинаково справа налево и слева направо)
+с использованием рекурсии.
+"""
+
+
+def is_palindrome(string, index=0):
+    if index <= len(string) // 2:
+        if string[index] != string[len(string) - 1 - index]:
+            return 'Не палиндром'
+        is_palindrome(string, index + 1)
+    return 'Палиндром'
+
+
+print(is_palindrome('12321'))  # Палиндром
+print(is_palindrome('123456'))
